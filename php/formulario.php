5521aba3d563abe9180c3b41eff4cac9cef32006
Jandueza <jonathanandueza1201@gmail.com>
2023-12-05 15:49:31 -0300

    sql connection & form data post

---
+++ php/formulario.php
@@ -0,0 +1,28 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+$apellido = $_POST['apellido'];
+
+$tema = $_POST['tema'];
+
+$conexion = mysqli_connect("localhost", "root", "root", "integradorphp");
+
+
+if ($nombre == "" || $apellido == "" || $tema == "") {
+    echo "Error, debe completar todos los campos";
+    exit();
+} else {
+    $sql = "INSERT INTO oradores (nombre, apellido, tema) VALUES ('$nombre', '$apellido', '$tema');";
+    $resultado = mysqli_query($conexion, $sql);
+}
+
+if ($resultado) {
+    echo "Se ha enviado el formulario correctamente";
+} else {
+    echo "Error al enviar el formulario";
+}
+
+mysqli_close($conexion);
+
+?>
